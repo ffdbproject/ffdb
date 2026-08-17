75a7b37fd80fbb862df65a8072b6087a23a8f0b2
Tanvir Rana Rabbi <tanvirranarabbi2@gmail.com>
2026-08-17 19:26:08 +0600

    Delete backend directory

--- backend/src/routes/team.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const { getPublicTeam } = require('../controllers/teamController');
-
-// GET /api/team
-router.get('/', getPublicTeam);
-
-module.exports = router;
